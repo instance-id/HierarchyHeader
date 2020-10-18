9fdce06b4eefe8453f142f564d091e324ae308f3
instance.id <dan@instance.id>
2020-10-17 23:39:17 -0500

    Testing UPM setup

---
+++ Editor/VersionConstants.cs
@@ -0,0 +1,33 @@
+namespace instance.id.HierarchyHeader.Editor
+{
+	/// <summary>
+	/// Version info for this library.
+	/// </summary>
+	internal static class VersionConstants
+	{
+		/// <summary>
+		/// The semantic version
+		/// </summary>
+		public const string VERSION = "0.0.1";
+
+		/// <summary>
+		/// The branch of GIT this package was published from.
+		/// </summary>
+		public const string GIT_BRANCH = "main";
+
+		/// <summary>
+		/// The current GIT commit hash this package was published on.
+		/// </summary>
+		public const string GIT_COMMIT = "1fac352123050c067d802e760115d7d9e5b09df8";
+
+		/// <summary>
+		/// The UTC human-readable date this package was published at.
+		/// </summary>
+		public const string PUBLISH_DATE = "Sunday, October 18, 2020";
+
+		/// <summary>
+		/// The UTC time this package was published at.
+		/// </summary>
+		public const string PUBLISH_TIME = "10/18/2020 04:37:55";
+	}
+}
